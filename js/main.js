324f9fda203671796d9d35e0ee8818e323cebfa0
rosario <58920070+athena272@users.noreply.github.com>
2025-12-24 14:29:01 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,19 @@
-import { createDateElement, createListItem, verificarListaVazia } from './dom.js';
+import { createDateElement, createListItem } from './dom.js';
 import buildDateLabel from './datetime.js';
+import checkEmptyList from './checkEmptyList.js';
 
-const inputItem = document.getElementById('input-item');
 const shopList = document.getElementById('lista-de-compras');
-// const addButton = document.getElementById('adicionar-item');
 const formShopList = document.getElementById('form-shoplist');
-
-let contador = 0;
 
 formShopList.addEventListener('submit', (event) =>
 {
     event.preventDefault();
-    if (!inputItem.value)
-    {
-        alert('Por favor, insira um item!');
-        return;
-    }
-
-    const { li, checkbox, name } = createListItem(inputItem.value, `checkbox-${++contador}`);
-    shopList.appendChild(li);
-
-    checkbox.addEventListener('click', () =>
-    {
-        name.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
-    });
-
     const dataElement = createDateElement(buildDateLabel());
+
+    const { li } = createListItem();
     li.appendChild(dataElement);
 
-    verificarListaVazia(shopList);
-});
+    shopList.appendChild(li);
 
-verificarListaVazia(shopList);
+    checkEmptyList(shopList);
+});
